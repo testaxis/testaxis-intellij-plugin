303b48763963e9b55b5f59253719dc8d01211a72
Casper Boone <mail@casperboone.nl>
2020-11-10 19:45:16 +0100

    Add test case details UI test

--- src/test/kotlin/io/testaxis/intellijplugin/IntelliJPlatformUITest.kt
+++ src/test/kotlin/io/testaxis/intellijplugin/IntelliJPlatformUITest.kt
@@ -3,10 +3,13 @@ package io.testaxis.intellijplugin
 import org.assertj.core.api.Assertions
 import org.assertj.swing.core.GenericTypeMatcher
 import org.assertj.swing.edt.FailOnThreadViolationRepaintManager
+import org.assertj.swing.edt.GuiActionRunner
 import org.assertj.swing.exception.ComponentLookupException
 import org.assertj.swing.fixture.FrameFixture
+import org.jetbrains.kotlin.tools.projectWizard.wizard.ui.textField
 import org.junit.jupiter.api.BeforeAll
 import javax.swing.JLabel
+import javax.swing.text.JTextComponent
 
 abstract class IntelliJPlatformUITest : IntelliJPlatformTest() {
     companion object {
@@ -17,15 +20,43 @@ abstract class IntelliJPlatformUITest : IntelliJPlatformTest() {
         }
     }
 
+    protected fun <T> executeGuiAction(query: () -> T): T {
+        return GuiActionRunner.execute<T> { query() }
+    }
+
     protected fun FrameFixture.requireContainsLabel(requiredText: String) {
+        var found = false
+
         val textMatcher: GenericTypeMatcher<JLabel> = object : GenericTypeMatcher<JLabel>(JLabel::class.java) {
-            override fun isMatching(label: JLabel) = requiredText == label.text
+            override fun isMatching(label: JLabel): Boolean {
+                if (found) {
+                    return false
+                }
+
+                if (requiredText == label.text) {
+                    found = true
+                }
+
+                return requiredText == label.text
+            }
         }
 
         try {
             label(textMatcher).requireVisible()
         } catch (exception: ComponentLookupException) {
-            Assertions.fail("Label with \"$requiredText\" is not present in the frame.")
+            Assertions.fail("Label with \"$requiredText\" is not present in the frame.", exception)
+        }
+    }
+
+    protected fun FrameFixture.requireContainsTextBox(requiredText: String) {
+        val textMatcher: GenericTypeMatcher<JTextComponent> = object : GenericTypeMatcher<JTextComponent>(JTextComponent::class.java) {
+            override fun isMatching(label: JTextComponent) = requiredText == label.text
+        }
+
+        try {
+            textBox(textMatcher).requireVisible()
+        } catch (exception: ComponentLookupException) {
+            Assertions.fail("Text box with \"$requiredText\" is not present in the frame.")
         }
     }
 }
---
+++ src/test/kotlin/io/testaxis/intellijplugin/toolwindow/builds/views/TestCaseDetailsRightViewTest.kt
@@ -0,0 +1,43 @@
+package io.testaxis.intellijplugin.toolwindow.builds.views
+
+import io.testaxis.intellijplugin.FakeApiService
+import io.testaxis.intellijplugin.Fakes
+import io.testaxis.intellijplugin.IntelliJPlatformUITest
+import io.testaxis.intellijplugin.fakeTestCaseExecution
+import io.testaxis.intellijplugin.fakeTestCaseExecutionDetails
+import io.testaxis.intellijplugin.models.TestCaseExecution
+import org.assertj.swing.fixture.Containers
+import org.junit.jupiter.api.Test
+
+class TestCaseDetailsRightViewTest : IntelliJPlatformUITest() {
+    val fakeDetails = fakeTestCaseExecutionDetails(
+        failureMessage = "io.testaxis.backend.exceptions.ResourceNotFoundException",
+        failureType = "AssertionError",
+        failureContent = "io.testaxis.backend.exceptions.ResourceNo[[[OMITTED]]]Thread.java:834)\n",
+    )
+
+    override fun getFakes() = Fakes(
+        apiService = object : FakeApiService() {
+            override suspend fun getTestCaseExecutionDetails(testCaseExecution: TestCaseExecution) = fakeDetails
+        }
+    )
+
+    @Test
+    fun `it shows test case execution details`() {
+        val testCaseDetailsRightView = executeGuiAction { TestCaseDetailsRightView(fixture.project) }
+        val frame = Containers.showInFrame(testCaseDetailsRightView.getPanel())
+
+        executeGuiAction {
+            testCaseDetailsRightView.setTestCaseExecution(fakeTestCaseExecution())
+        }
+
+        frame.requireContainsLabel("it can shorten a commit hash to be human readable")
+        frame.requireContainsLabel("com.example.service")
+
+        frame.requireContainsTextBox("io.testaxis.backend.exceptions.ResourceNotFoundException")
+        frame.requireContainsLabel("AssertionError")
+        frame.requireContainsTextBox("io.testaxis.backend.exceptions.ResourceNo[[[OMITTED]]]Thread.java:834)\n")
+
+        executeGuiAction { testCaseDetailsRightView.dispose() }
+    }
+}
