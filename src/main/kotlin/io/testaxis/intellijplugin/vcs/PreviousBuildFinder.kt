e0a157533e7e417b3b4144c35913c37980f4a751
Casper Boone <mail@casperboone.nl>
2020-12-07 14:13:30 +0100

    Remove commit hashes print statement

--- src/main/kotlin/io/testaxis/intellijplugin/vcs/PreviousBuildFinder.kt
+++ src/main/kotlin/io/testaxis/intellijplugin/vcs/PreviousBuildFinder.kt
@@ -29,7 +29,5 @@ fun Build.findPreviousBuild(project: Project, builds: List<Build>): Build? =
             .map { it.id.asString() }
             .drop(1) // first one is the build commit
 
-        println("Commit hashes history: $commitHashesHistory")
-
         builds.find { commitHashesHistory.contains(it.commit) }
     }
